6dd26250c7e803cc5e8f615a6eb6e58c33bb2149
Dipankar Sarkar <dipankarsarkar@gmail.com>
2014-03-14 15:28:47 +0530

    Added more code

--- mobiletest.js
+++ mobiletest.js
@@ -4,8 +4,10 @@ Handlebars.registerHelper('eachProperty', function(context, options) {
     var ret = "";
     for(var prop in context)
     {
+       if(prop!="image" && prop!="height") {
             ret = ret + options.fn({property:prop,value:context[prop]});
-        }
+       }
+   }
     return ret;
 });
 
@@ -14,12 +16,12 @@ Handlebars.registerHelper('eachProperty', function(context, options) {
   Template.hello.events({
     'click button': function (e) {
        var url = $("#url").val();
-       Session.set("normal",{"loading":"..."});
-       Session.set("mobile",{"loading":"..."});
+       Session.set("normal",{"loading":"...","height":"0"});
+       Session.set("mobile",{"loading":"...","height":"0"});
        Meteor.call("crawl",url, "normal",function(err,res){   
           Session.set("normal",res);
        });
-       Meteor.call("crawl","http://173.193.215.18/?url="+url, "mobile",function(err,res){
+       Meteor.call("crawl",url, "mobile",function(err,res){
           Session.set("mobile",res);
       });
     }
@@ -33,16 +35,24 @@ if (Meteor.isServer) {
             var fut = new Future();
             console.log(url);
             var pname = CryptoJS.MD5(url).toString() + ".png";
-                  phantomas(url,{"phone":true,"analyze-css":true,"user-agent":"Mozilla/5.0 (Linux; U; Android 2.1-update1; en-us; SAMSUNG-SGH-I897/I897UCJH7 Build/ECLAIR) AppleWebKit/530.17 (KHTML, like Gecko) Version/4.0 Mobile Safari/530.17","viewport":"480x800","screenshot":"/home/dipankar/Projects/daily/mobiletest/public/"+pname},function(err,res){
+            var params = {"analyze-css":true,"user-agent":"Mozilla/5.0 (Linux; U; Android 2.1-update1; en-us; SAMSUNG-SGH-I897/I897UCJH7 Build/ECLAIR) AppleWebKit/530.17 (KHTML, like Gecko) Version/4.0 Mobile Safari/530.17","viewport":"480x800","screenshot":"/tmp/"+pname};
+            if(t=="mobile") {
+               params["proxy"] = "173.193.215.18:8001";
+            }
+            phantomas(url,params,function(err,res){
+               var image_data = fs.readFileSync("/tmp/"+pname);
+               var image_b64 = new Buffer(image_data).toString('base64');
                console.log("Done");
                var ret = res["metrics"];
-               ret["image"] = pname;
+               ret["image"] = image_b64;
+               ret["height"] = "800";
                fut.return(res["metrics"]);
             });
             return fut.wait();
       }
   });
   Meteor.startup(function () {
+     fs = Npm.require('fs');
      Future = Npm.require('fibers/future');
      phantomas = Meteor.require('phantomas');
     // code to run on server at startup
